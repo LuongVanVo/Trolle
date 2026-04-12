83ff67baac800160adb9a3acbc4f5c7e3424b625
vanvo299 <luongvanvo29@gmail.com>
2026-04-12 21:49:32 +0700

    refactor: clean up nginx and Vite configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,5 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/react-app/'
 })
